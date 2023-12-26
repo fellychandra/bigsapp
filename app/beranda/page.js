cef6040aa3369ea296ce194a784897e1e6c1b8b9
Felly Chandra <felly20ti@mahasiswa.pcr.ac.id>
2023-12-26 21:19:23 +0700

    dokter

--- app/beranda/page.js
+++ app/beranda/page.js
@@ -1,3 +1,4 @@
+'use client';
 import Card from '@/components/card'
 import Logo from '@/components/logo'
 import Menu from '@/components/menu'
@@ -16,10 +17,10 @@ export default function Beranda() {
                     <div className='py-2'>
                         <div className='text-base font-bold pb-1'>Menu</div>
                         <div className='grid grid-cols-2 gap-3  place-items-center justify-center py-[9px] px-2 sm:px-2 bg-white h-[213px] w-full shadow-custom rounded-[5px]'>
-                            <Menu style={'bg-pink1 h-[90.549px] w-full'} teks={"Poliklinik"} link={"/beranda/poliklinik"} />
-                            <Menu style={'bg-pink2 h-[90.549px] w-full'} teks={"Info Layanan"} link={"/beranda//info-layanan"} />
-                            <Menu style={'bg-primary h-[90.549px] w-full'} teks={"BMI"} link={"/beranda//bmi"}/>
-                            <Menu style={'bg-ungu1 h-[90.549px] w-full'} teks={"Kritik & Saran"} link={"/beranda//kritik-saran"} />
+                            <Menu style={'bg-pink1 h-full w-full'} teks={"Poliklinik"} link={"/beranda/poliklinik"} />
+                            <Menu style={'bg-pink2 h-full w-full'} teks={"Info Layanan"} link={"/beranda//info-layanan"} />
+                            <Menu style={'bg-primary h-full w-full'} teks={"BMI"} link={"/beranda//bmi"} />
+                            <Menu style={'bg-ungu1 h-full w-full'} teks={"Kritik & Saran"} link={"/beranda//kritik-saran"} />
                         </div>
                     </div>
                 </section>
--- components/card/index.js
+++ components/card/index.js
@@ -1,3 +1,4 @@
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 const Card = ({ style, data }) => {
@@ -6,16 +7,30 @@ const Card = ({ style, data }) => {
         { url: 'https://images.unsplash.com/photo-1527613426441-4da17471b66d?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
         { url: 'https://images.unsplash.com/photo-1682687219800-bba120d709c5?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
     ];
+    const pathName = usePathname();
+
+
     return (
         <>
-            <div className={`${style} flex px-[9px] shadow-custom rounded-[5px] cursor-pointer`}>
-                <div style={{ backgroundImage: `url(${slides[0].url})` }} className='rounded-full w-12 h-12 bg-center bg-cover flex justify-center'>
+            <div className={`${style} grid grid-cols-1 p-[9px] shadow-custom rounded-[5px] cursor-pointer`}>
+                <div className="flex items-center">
+                    <div style={{ backgroundImage: `url(${slides[0].url})` }} className='rounded-full w-12 h-12 bg-center bg-cover flex justify-center'>
+                    </div>
+                    <div className='items-center ml-2'>
+                        <div className='font-bold text-sm'>dr. Felly Chandra Ganteng Kali </div>
+                        <div className='text-abutext font-normal text-[12px]'>Klinik jaya abadi </div>
+                    </div>
                 </div>
-                <div className='items-center ml-3'>
-                    <div className='font-bold text-sm overflow-hidden'>dr. Felly Chandra Ganteng Kali </div>
-                    <div className='text-abutext font-normal text-[11px] overflow-hidden'>Klinik jaya abadi </div>
-                </div>
-            </div>
+                {
+                    pathName.includes('dokter') && (
+                        <div className='flex justify-between pt-1'>
+                            <div className='bg-primary1 p-1 rounded-[5px] font-normal text-[12px] text-white'>Lihat Jadwal</div>
+                            <div className='bg-primary1 p-1 rounded-[5px] font-normal text-[12px] text-white'>Buat Janji</div>
+                        </div>
+                    )
+                }
+
+            </div >
         </>
     )
 }
