12442f63a8ac515b37ddb3dedef3d2608236a437
Felly Chandra <felly20ti@mahasiswa.pcr.ac.id>
2023-12-26 12:52:55 +0700

    warna putih

--- components/menu/index.js
+++ components/menu/index.js
@@ -4,7 +4,7 @@ const Menu = ({ style,  teks }) => {
     return (
         <>
             <div className={`${style} flex shadow-custom rounded-[5px] overflow-hidden cursor-pointer`}>
-                <div className='w-[210px] h-[120px] translate-x-16 -translate-y-12 rotate-45 bg-cover bg-black flex justify-center'>
+                <div className='w-[210px] h-[120px] translate-x-16 -translate-y-12 rotate-45 bg-cover bg-white/20 flex justify-center'>
                     <span className='text-white font-bold'>{teks}</span>
                 </div>
             </div>
