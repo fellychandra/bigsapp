3f28ea44f119ad819280bba4ea8f9b7c9742448e
Felly Chandra <felly20ti@mahasiswa.pcr.ac.id>
2024-03-05 14:07:37 +0700

    detail riwayat

--- app/riwayat/[detail_riwayat]/page.js
+++ app/riwayat/[detail_riwayat]/page.js
@@ -11,7 +11,7 @@ const DetailRiwayat = ({ params }) => {
       <div className='bg-primary1 w-full h-[200px] -z-10 absolute top-0 left-0 right-0'></div>
       <div className='px-2'>
         <section className='mt-5'>
-          <Link href={'/beranda'} className='flex items-center p-1 font-bold text-lg gap-1 text-white'>
+          <Link href={'/'} className='flex items-center p-1 font-bold text-lg gap-1 text-white'>
             <CaretLeft size={18} weight='bold' />
             <div className=''>
               Riwayat Pelayanan
@@ -22,7 +22,7 @@ const DetailRiwayat = ({ params }) => {
           <div className="flex flex-col gap-2">
             <div className="grid grid-cols-1 gap-1 text-sm">
               <div className="border p-2 grid gap-2 bg-white rounded">
-                <div className="border-b">
+                <div className="border-b font-medium">
                   Keluhan :
                 </div>
                 <div className='text-kecil'>
@@ -30,7 +30,7 @@ const DetailRiwayat = ({ params }) => {
                 </div>
               </div>
               <div className="border p-2 grid gap-2 bg-white rounded">
-                <div className="border-b">
+                <div className="border-b font-medium">
                   Observasi :
                 </div>
                 <div className="grid grid-cols-2 gap-1 text-kecil">
@@ -39,7 +39,7 @@ const DetailRiwayat = ({ params }) => {
                       167 cm
                     </div>
                   </div>
-                  <div className="pb-1">berat Badan :
+                  <div className="pb-1">Berat Badan :
                     <div>
                       56 kg
                     </div>
@@ -95,7 +95,7 @@ const DetailRiwayat = ({ params }) => {
                 </div>
               </div>
               <div className="border p-2 grid gap-2 bg-white rounded">
-                <div className="border-b">Diagnosa :</div>
+                <div className="border-b font-medium">Diagnosa :</div>
                 <div className="grid grid-cols-2 gap-1 text-kecil">
                   <div className="pb-1">Premier:
                     <div>
@@ -116,7 +116,7 @@ const DetailRiwayat = ({ params }) => {
                 </div>
               </div>
               <div className="border p-2 grid gap-2 bg-white rounded">
-                <div className="border-b">
+                <div className="border-b font-medium">
                   Resep Obat :
                 </div>
                 <div className='text-kecil'>
@@ -124,7 +124,7 @@ const DetailRiwayat = ({ params }) => {
                 </div>
               </div>
               <div className="border p-2 grid gap-2 bg-white rounded">
-                <div className="border-b">
+                <div className="border-b font-medium">
                   Tindakan :
                 </div>
                 <div className='text-kecil'>
